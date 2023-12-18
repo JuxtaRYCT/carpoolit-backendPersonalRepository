1bf27e5af4be9b89e6455930d795b964e8237b8c
Harshitaa Kashyap <suzimayisbae@gmail.com>
2023-12-19 01:47:06 +0530

    feat: UpdateRide route

--- main.go
+++ main.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"carpool-backend/database"
-	"github.com/gofiber/fiber/v2"
+	routes "carpool-backend/routes/rides"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
 
 	database.ConnectToDB()
-
+	setUpRoutes(app)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
-
 	log.Fatal(app.Listen(":3000"))
+}
+
+func setUpRoutes(app *fiber.App) {
+	app.Put("/api/ride/:id", routes.UpdateRide)
 }
