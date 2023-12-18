fe54cdc618611fef13ae3c54c7188200666b9711
Tanush Golwala <tanushgolwala@gmail.com>
2023-12-18 23:49:16 +0530

    fix: added comments

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+// contents of all the routes will be imported here
 func SetupRoutes(app *fiber.App) {
 	app.Post("/booking", bookings.CreateBooking)
 	// app.Post("/ride", rides.CreateRide)
@@ -22,7 +23,7 @@ func main() {
 	app := fiber.New()
 	database.ConnectToDB()
 
-	SetupRoutes(app)
+	SetupRoutes(app) //setting up routes
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
--- routes/bookings/CreateBooking.go
+++ routes/bookings/CreateBooking.go
@@ -10,32 +10,37 @@ import (
 )
 
 func CreateBooking(c *fiber.Ctx) error {
-
+	// Define a variable to hold the Booking struct
 	var Booking models.Booking
-	err := c.BodyParser(&Booking)
 
+	// Parse the request body into the Booking struct
+	err := c.BodyParser(&Booking)
 	if err != nil {
 		log.Printf("Error parsing JSON: %v\n", err)
 		return c.Status(400).SendString("Error parsing JSON")
 	}
 
+	// Validate the Booking struct
 	if err := helpers.ValidateBooking(Booking); err != nil {
 		log.Printf("Error validating Booking: %v\n", err)
 		return c.Status(400).SendString("Error validating Booking")
 	}
+
+	// Check if the user has already booked the ride
 	existingBooking := models.Booking{}
 	if database.Database.Db.Where("user_id = ? AND ride_id = ?", Booking.PassengerID, Booking.RideID).First(&existingBooking).Error == nil {
 		log.Println("Similar booking already exists")
 		return c.Status(400).SendString("Similar booking already exists")
 	}
 
+	// Insert the booking into the database
 	result := database.Database.Db.Create(&Booking)
-
 	if result.Error != nil {
 		log.Printf("Error creating booking: %v\n", result.Error)
 		return c.Status(500).SendString("Error creating booking")
 	}
 
+	// Log the booking id
 	log.Printf("Booking with id %v created\n", Booking.ID)
 	return c.Status(200).JSON(Booking)
 }
