da08d89a2c6a6828cb12ca4b74e646c5d8feea42
Harshitaa Kashyap <suzimayisbae@gmail.com>
2023-12-19 15:04:03 +0530

    fix: removed UpdateRide struct and added logs

--- routes/rides/UpdateRide.go
+++ routes/rides/UpdateRide.go
@@ -24,42 +24,34 @@ func UpdateRide(c *fiber.Ctx) error {
 	id, err := strconv.Atoi(c.Params("id")) // used strconv.Atoi
 	var ride models.Ride
 	if err != nil || id <= 0 { // Check provided id is correct or not, displays error if incorrect id
+		log.Printf("Invalid or missing ride ID: %v", err)
 		return c.Status(400).JSON("Please provide valid ID")
 	}
 	if err := database.Database.Db.Find(&ride, id).Error; err != nil { // Find ride via id, displays error if ride not fetched
+		log.Printf("Error fetching the ride with ID %d: %v", id, err)
 		return c.Status(400).JSON("Error fetching the ride: " + err.Error())
+	}
+	// Parse and decode the JSON request body into the ride struct
+	if err := c.BodyParser(&ride); err != nil {
+		log.Printf("Error parsing JSON request body: %v", err)
+		return c.Status(400).JSON("Error parsing JSON: " + err.Error())
 	}
 	if err := helpers.ValidateRide(ride); err != nil { // validates ride, display error if not validated
 		log.Printf("Error validating ride: %v\n", err)
 		return c.Status(400).SendString("Error validating ride")
 	}
-	type UpdateRide struct { // struct for ride update
-		HostUser      models.User `json:"host_user"`
-		StartLocation string      `json:"start_location"`
-		EndLocation   string      `json:"end_location"`
-		TotalSeats    uint        `json:"total_seats"`
-		BookedSeats   uint        `json:"booked_seats"`
-		TotalPrice    uint        `json:"total_price"`
-	}
-	var updateData UpdateRide // creates variable updateData to store data of UpdateRide
-
-	if err := c.BodyParser(&updateData); err != nil { // parse the request body and decode it into the updateData variable, display error if unable to do so.
-		return c.Status(500).JSON(err.Error())
-	}
-	// Update ride properties with new data.
-	ride.HostUser = updateData.HostUser
-	ride.StartLocation = updateData.StartLocation
-	ride.EndLocation = updateData.EndLocation
-	ride.TotalSeats = updateData.TotalSeats
-	ride.BookedSeats = updateData.BookedSeats
-	ride.TotalPrice = updateData.TotalPrice
-
-	// Save the updated ride to the database.
+	// Save the ride to the database.
 	updatedRide := database.Database.Db.Save(&ride)
-	if updatedRide.RowsAffected == 0 {
-		return c.Status(500).SendString("No ride updated")
+	// Check for errors during the database update
+	if updatedRide.Error != nil {
+		log.Printf("Error updating ride with ID %d: %v", ride.ID, updatedRide.Error)
+		return c.Status(500).SendString("Error updating ride")
 	}
-
+	// Check if any rows were affected during the update
+	if updatedRide.RowsAffected == 0 {
+		log.Printf("Ride with ID %d not found", ride.ID)
+		return c.Status(500).SendString("Ride not found")
+	}
 	// Log success and return the updated ride as JSON.
 	log.Printf("Ride with ID %d has been updated", ride.ID)
 	return c.Status(200).JSON(ride)
