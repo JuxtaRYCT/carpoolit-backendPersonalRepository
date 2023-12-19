82772154f661a9f3319a341a2f45fc852bbe3dd9
Harshitaa Kashyap <suzimayisbae@gmail.com>
2023-12-19 13:48:37 +0530

    fix: comments

--- routes/rides/UpdateRide.go
+++ routes/rides/UpdateRide.go
@@ -11,20 +11,29 @@ import (
 )
 
 // UpdateRide handles updating a ride based on the provided ID in the Fiber context.
+// Accepts a JSON object containing the ride information
+// Returns an HTTP status code and a message indicating the result of the operation
+//
+// Parameters:
+//   - c: Fiber context
+//
+// Returns:
+//   - error: An error message indicating the result of the operation and an HTTP status code
+
 func UpdateRide(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id"))
+	id, err := strconv.Atoi(c.Params("id")) // used strconv.Atoi
 	var ride models.Ride
-	if err != nil || id <= 0 {
+	if err != nil || id <= 0 { // Check provided id is correct or not, displays error if incorrect id
 		return c.Status(400).JSON("Please provide valid ID")
 	}
-	if err := database.Database.Db.Find(&ride, id).Error; err != nil {
+	if err := database.Database.Db.Find(&ride, id).Error; err != nil { // Find ride via id, displays error if ride not fetched
 		return c.Status(400).JSON("Error fetching the ride: " + err.Error())
 	}
-	if err := helpers.ValidateRide(ride); err != nil {
+	if err := helpers.ValidateRide(ride); err != nil { // validates ride, display error if not validated
 		log.Printf("Error validating ride: %v\n", err)
 		return c.Status(400).SendString("Error validating ride")
 	}
-	type UpdateRide struct {
+	type UpdateRide struct { // struct for ride update
 		HostUser      models.User `json:"host_user"`
 		StartLocation string      `json:"start_location"`
 		EndLocation   string      `json:"end_location"`
@@ -32,9 +41,9 @@ func UpdateRide(c *fiber.Ctx) error {
 		BookedSeats   uint        `json:"booked_seats"`
 		TotalPrice    uint        `json:"total_price"`
 	}
-	var updateData UpdateRide
+	var updateData UpdateRide // creates variable updateData to store data of UpdateRide
 
-	if err := c.BodyParser(&updateData); err != nil {
+	if err := c.BodyParser(&updateData); err != nil { // parse the request body and decode it into the updateData variable, display error if unable to do so.
 		return c.Status(500).JSON(err.Error())
 	}
 	// Update ride properties with new data.
