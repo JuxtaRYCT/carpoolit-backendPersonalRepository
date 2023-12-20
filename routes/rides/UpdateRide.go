7182a997b59db4413d036f239bf71827016d6ba3
Harshitaa Kashyap <suzimayisbae@gmail.com>
2023-12-20 12:13:03 +0530

    fix: removed parameter parsing

--- routes/rides/UpdateRide.go
+++ routes/rides/UpdateRide.go
@@ -5,7 +5,6 @@ import (
 	"carpool-backend/helpers"
 	"carpool-backend/models"
 	"log"
-	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -21,38 +20,36 @@ import (
 //   - error: An error message indicating the result of the operation and an HTTP status code
 
 func UpdateRide(c *fiber.Ctx) error {
-	id, err := strconv.Atoi(c.Params("id")) // used strconv.Atoi
-	var ride models.Ride
-	if err != nil || id <= 0 { // Check provided id is correct or not, displays error if incorrect id
-		log.Printf("Invalid or missing ride ID: %v", err)
-		return c.Status(400).JSON("Please provide valid ID")
-	}
-	if err := database.Database.Db.Find(&ride, id).Error; err != nil { // Find ride via id, displays error if ride not fetched
-		log.Printf("Error fetching the ride with ID %d: %v", id, err)
-		return c.Status(400).JSON("Error fetching the ride: " + err.Error())
-	}
+	var updatedRide models.Ride
+
 	// Parse and decode the JSON request body into the ride struct
-	if err := c.BodyParser(&ride); err != nil {
-		log.Printf("Error parsing JSON request body: %v", err)
+	if err := c.BodyParser(&updatedRide); err != nil {
+		log.Printf("Error parsing JSON request body: %v\n", err)
 		return c.Status(400).JSON("Error parsing JSON: " + err.Error())
 	}
-	if err := helpers.ValidateRide(ride); err != nil { // validates ride, display error if not validated
+
+	// validates ride, display error if not validated
+	if err := helpers.ValidateRide(updatedRide); err != nil {
 		log.Printf("Error validating ride: %v\n", err)
 		return c.Status(400).SendString("Error validating ride")
 	}
-	// Save the ride to the database.
-	updatedRide := database.Database.Db.Save(&ride)
+
+	// Update the ride to the database, using id.
+	result := database.Database.Db.Model(&models.Ride{}).Where("id = ?", updatedRide.ID).Updates(&updatedRide)
+
 	// Check for errors during the database update
-	if updatedRide.Error != nil {
-		log.Printf("Error updating ride with ID %d: %v", ride.ID, updatedRide.Error)
+	if result.Error != nil {
+		log.Printf("Error updating ride: %v\n", result.Error)
 		return c.Status(500).SendString("Error updating ride")
 	}
+
 	// Check if any rows were affected during the update
-	if updatedRide.RowsAffected == 0 {
-		log.Printf("Ride with ID %d not found", ride.ID)
-		return c.Status(500).SendString("Ride not found")
+	if result.RowsAffected == 0 {
+		log.Printf("Ride with ID %v not found\n", updatedRide.ID)
+		return c.Status(404).SendString("Ride not found")
 	}
+
 	// Log success and return the updated ride as JSON.
-	log.Printf("Ride with ID %d has been updated", ride.ID)
-	return c.Status(200).JSON(ride)
+	log.Printf("Ride with ID %v has been updated", updatedRide.ID)
+	return c.Status(200).JSON(updatedRide)
 }
