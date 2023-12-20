a8ab59fa6e3b68f9cb454901c5bde3383b96d470
eshita241 <eshitachokhani2003@gmail.com>
2023-12-20 13:43:09 +0530

    feat:EditBooking

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"carpool-backend/database"
-	"github.com/gofiber/fiber/v2"
+	"carpool-backend/routes/bookings"
+	"carpool-backend/routes/rides"
+	"carpool-backend/routes/users"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
@@ -11,9 +15,18 @@ func main() {
 
 	database.ConnectToDB()
 
+	SetupRoutes(app)
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World 👋!")
 	})
 
 	log.Fatal(app.Listen(":3000"))
+}
+
+func SetupRoutes(app *fiber.App) {
+	// booking routes
+	app.Put("/bookings/:id", bookings.EditBooking)
+	app.Post("/bookings", bookings.CreateBooking)
+	app.Post("/api/user", users.CreateUser)
+	app.Post("/api/ride", rides.CreateRide)
 }
