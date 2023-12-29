bcd9e911b59d5121a71cb3b6cf873e2d22068449
RohanKhatua <contact@rohankhatua.dev>
2023-12-29 12:59:38 +0530

    feat: update bookings schema

--- models/BookingModel.go
+++ models/BookingModel.go
@@ -7,10 +7,11 @@ import (
 // Booking is a representation of a booking in the database
 type Booking struct {
 	gorm.Model
-	RideID      uint `gorm:"not null;uniqueIndex:idx_booking_ride_passenger" json:"ride_id" valid:"required~Ride ID is required"`
-	Ride        Ride `gorm:"foreignKey:RideID;references:ID" json:"ride"`
-	PassengerID uint `gorm:"not null;uniqueIndex:idx_booking_ride_passenger" json:"passenger_id" valid:"required~Passenger ID is required"`
-	Passenger   User `gorm:"foreignKey:PassengerID;references:ID" json:"passenger"`
+	RideID        uint   `gorm:"not null;uniqueIndex:idx_booking_ride_passenger" json:"ride_id" valid:"required~Ride ID is required"`
+	Ride          Ride   `gorm:"foreignKey:RideID;references:ID" json:"ride"`
+	PassengerID   uint   `gorm:"not null;uniqueIndex:idx_booking_ride_passenger" json:"passenger_id" valid:"required~Passenger ID is required"`
+	Passenger     User   `gorm:"foreignKey:PassengerID;references:ID" json:"passenger"`
+	RequestStatus string `gorm:"type:varchar(10);not null" json:"request_status" valid:"in(accepted|pending|rejected)~Request Status is either accepted, pending or rejected,required~Status is Required"`
 }
 
 type BookingResponse struct {
