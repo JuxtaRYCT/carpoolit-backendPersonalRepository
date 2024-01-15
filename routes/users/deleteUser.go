816e2faa9758398177a3f76f2ec69606339344d9
Manan Shah <74666385+manan04shah@users.noreply.github.com>
2024-01-15 16:27:21 +0530

    Update deleteUser.go

--- routes/users/deleteUser.go
+++ routes/users/deleteUser.go
@@ -23,15 +23,15 @@ func DeleteUser(c *fiber.Ctx) error {
 	//Delete the user from the database
 	result := database.Database.Db.Delete(&models.User{}, userId)
 
-	if result.Error != nil {
-		log.Printf("Error deleting user: %v\n", result.Error)
-		return c.Status(500).SendString("Could not delete user - Internal Server Error")
-	} //If the user isn't deleted properly the function will return an error, and we'll know there was a problem during deletion
-
 	if result.RowsAffected == 0 {
 		log.Printf("Error deleting user: user not found - %v\n", userId)
 		return c.Status(404).SendString("user not found")
 	} //If none of the rows change ,then there was no user to be deleted, and we'll return a 404 status code
+
+	if result.Error != nil {
+		log.Printf("Error deleting user: %v\n", result.Error)
+		return c.Status(500).SendString("Could not delete user - Internal Server Error")
+	} //If the user isn't deleted properly the function will return an error, and we'll know there was a problem during deletion
 
 	log.Printf("User with id %v deleted", userId)
 	return c.Status(200).SendString("User deleted") // in case it is successfully deleted we'll print user deleted
